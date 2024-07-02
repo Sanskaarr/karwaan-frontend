d9063b3cad9d8c0393c13e8241125deb117d9122
priyanshuspawar <priyansusingh306@gmail.com>
2024-07-02 19:47:56 +0530

    fix: video page carousel

--- src/app/(pages)/(static)/videos/page.tsx
+++ src/app/(pages)/(static)/videos/page.tsx
@@ -1,18 +1,18 @@
 "use client";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import WestIcon from "@mui/icons-material/West";
 import EastIcon from "@mui/icons-material/East";
-import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
-import { useRouter } from "next/navigation";
+import PlayCircleOutlinedIcon from '@mui/icons-material/PlayCircleOutlined';
 import videoData from "@/constants/VideoData";
 import { motion } from "framer-motion";
 import styles from "./style.module.css";
-import useEmblaCarousel from "embla-carousel-react";
-import { EmblaCarouselType } from "embla-carousel";
+import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import { useRouter } from "next/navigation";
 
 export default function Videos() {
-  const router = useRouter();
-
   const videosPageOptions = [
     { id: 0, OptionName: "Short Films" },
     { id: 1, OptionName: "Documentry" },
@@ -23,134 +23,136 @@ export default function Videos() {
   ];
 
   const handleMouseEnter = (e: any) => {
+    setIsVideoPlaying(true)
     e.target.play();
+  
   };
+  const router = useRouter();
 
   const handleMouseLeave = (e: any) => {
-    e.target.pause();
+    setIsVideoPlaying(false)
+    e.target.pause(); 
   };
-
-  const [emblaRef, emblaApi] = useEmblaCarousel(
-    {
-      // startIndex: videoData.length-1,
-      breakpoints: {
-        "(min-width: 684px)": { startIndex: 0, axis: "y", align: "center" },
-        "(min-width: 200px)": { startIndex: 0, axis: "y", align: "center" },
-        "(min-width: 1028px)": {
-          direction: "rtl",
-          axis: "x",
-          align: "end",
-          duration: 35,
-          // startIndex:videoData.length-1
-        },
-      },
-    },
-    [WheelGesturesPlugin()]
-  );
-  const onClickRightScroll = () => {
-    emblaApi?.scrollPrev();
-  };
-  const onClickLeftScroll = () => {
-    emblaApi?.scrollNext();
-  };
+  const [isVideoPlaying,setIsVideoPlaying] = useState(false)
   const [selectedSnap, setSelectedSnap] = useState(0);
-  const updateScrollSnapState = useCallback((emblaApi: EmblaCarouselType) => {
-    if (videoData.length >= emblaApi.selectedScrollSnap()) {
-      setSelectedSnap(emblaApi.selectedScrollSnap());
+  const onClickLeftScroll = () =>{
+    
+    swiperRef.current?.swiper.slideNext()
+  }
+  const onClickRightScroll = () =>{
+    swiperRef.current?.swiper.slidePrev()
+  }
+  const swiperRef = useRef<SwiperRef>(null)
+  useEffect(()=>{
+    if(typeof window !== 'undefined'){
+      if(window.innerWidth<1028){
+        swiperRef.current?.swiper.changeLanguageDirection("ltr")
+      }
     }
-  }, []);
-  useEffect(() => {
-    if (!emblaApi) return;
-
-    updateScrollSnapState(emblaApi);
-    emblaApi.on("select", updateScrollSnapState);
-    emblaApi.on("reInit", updateScrollSnapState);
-  }, [emblaApi, updateScrollSnapState]);
+  },[])
   return (
-    // <div></div>
-    <div className={styles.videos}>
-      <div className={styles.videosSection}>
-        <div className={styles.videosSectionHome}>
-          <span onClick={() => router.push("/")}>Home</span>
-        </div>{" "}
-        <motion.div
-          style={{ color: "black" }}
+   
+    <div style={{height:"90vh"}} className={styles.parentContainer}>
+              <Swiper
+              ref={swiperRef}
+              dir="rtl"
+              onSlidePrevTransitionEnd={()=>{
+                if(selectedSnap>0){
+                  setSelectedSnap(selectedSnap-1)}}
+                }
+              onSlideNextTransitionEnd={()=>{
+                if(selectedSnap<videoData.length){
+                  setSelectedSnap(selectedSnap+1)
+                }
+              }}
+              // onSlideChange={(s)=>{console.log(s.clickedIndex)}}
+              // dir="rtl"
+              breakpointsBase="window"
+              breakpoints={{200:{direction:"vertical",slidesPerView:3,spaceBetween:20},1029:{
+                slidesPerView:2,spaceBetween:50,centeredSlides:true,direction:"horizontal"
+              }}}
+              
+
+        
+        className={styles.swiper}
+      >
+        {
+          videoData.map((data,index)=>
+            <SwiperSlide key={data.id} className={styles.cardContainer}>
+
+         <div style={{display:"flex",flexGrow:1,justifyContent:"center",width:"100%",height:"100%",alignItems:"center"}}>
+         <div className={styles.slide}>
+         <motion.div
+           className={styles.slideChild}
+           initial={{ x: -100, y: 0, opacity: 0 }}
+           animate={{ x: 0, y: 0, opacity: 1 }}
+           transition={{ ease: "easeIn", delay: 1, duration: 2 }}
+          
+        
+         >
+                       <motion.div
+             style={{
+               cursor: "pointer",
+               width: "100%",
+               height: "100%",
+               position: "relative",
+               display: "flex",
+               overflow: "hidden",
+             }}
+           >
+         <motion.img
+               src={data.imgSrc}
+               alt={data.name}
+               className={styles.videosThumbnail}
+               initial={{ opacity: 0.3 }}
+               animate={{ opacity: 1 }}
+               transition={{ ease: "easeIn", delay: 0, duration: 0.3 }}
+             />
+             <video
+               onMouseEnter={handleMouseEnter}
+               onMouseLeave={handleMouseLeave}
+               autoPlay={true}
+               muted
+               loop
+               disablePictureInPicture
+               disableRemotePlayback
+               className={styles.videoContainer}
+             >
+               <source src={data.videoHref} type="video/mp4" />
+               Your browser does not support this feature
+             </video>
+             </motion.div>
+             </motion.div>
+         </div>
+         </div>
+</SwiperSlide>
+          )
+        }
+
+      </Swiper>
+      {/* <p style={{backgroundColor:"white",width:"100%",position:"absolute",textAlign:"center",bottom:0}}>Hold to play video</p> */}
+      <div className={styles.sidebarNav} >
+      <div className={styles.home}>
+           <span onClick={() => router.push("/")}>Home</span>
+         </div>{" "}
+{selectedSnap==0&&<motion.div
+          
           className={styles.motion}
           initial={{ x: 100, y: 0 }}
           animate={{ x: 0, y: 0 }}
+          exit={{opacity:0}}
           transition={{ ease: "easeIn", delay: 0, duration: 1 }}
         >
-          <span className={styles.videosPageOptionsTitle}>Motion</span>
+          <span style={{color:"black"}} className={styles.videosPageOptionsTitle}>Motion</span>
 
           {videosPageOptions.map((e) => (
             <span key={e.id} className={styles.optionName}>
               {e.OptionName}
-            </span>
-          ))}
-        </motion.div>
-        {/* <div style={{width:"100%",backgroundColor:"black",height:"100%"}}> */}
-        {videoData && videoData.length > 0 ? (
-          <div className={styles.embla} dir="rtl">
-            <div className={styles.emblaViewport} ref={emblaRef}>
-              <div className={styles.embla__container}>
-                {videoData.map((data: any, i) => {
-                  return (
-                    <motion.a
-                      className={styles.embla__slide}
-                      initial={{ x: -100, y: 0, opacity: 0 }}
-                      animate={{ x: 0, y: 0, opacity: 1 }}
-                      transition={{ ease: "easeIn", delay: 1, duration: 2 }}
-                      target="_blank"
-                      key={data.id}
-                      href={data.videoHref}
-                    >
-                      <motion.a
-                        style={{
-                          cursor: "pointer",
-                          width: "100%",
-                          height: "100%",
-                          position: "relative",
-                          display: "flex",
-                          overflow: "hidden",
-                        }}
-                      >
-                        {/* <div className={styles.videosThumbnail}></div> */}
-                        <motion.img
-                          src={data.imgSrc}
-                          alt={data.name}
-                          className={styles.videosThumbnail}
-                          // initial={{ opacity: 0.3 }}
-                          // animate={{ opacity: 1 }}
-                          // transition={{ ease: "easeIn", delay: 0, duration: 0.3 }}
-                        />
-                        <video
-                          onMouseEnter={handleMouseEnter}
-                          onMouseLeave={handleMouseLeave}
-                          autoPlay={false}
-                          muted
-                          loop
-                          className={styles.videoContainer}
-                        >
-                          <source src={data.videoHref} type="video/mp4" />
-                        </video>
-                      </motion.a>
-                    </motion.a>
-                  );
-                })}
-                <div className={styles.hideBox}>
-                  <p>sd</p>
-                </div>
-              </div>
+            </span>))}
+    </motion.div>}
             </div>
-          </div>
-        ) : (
-          <p>no data</p>
-        )}
-        {/* </div> */}
-      </div>
-
-      {/* Scroll Bar */}
-      <div className={styles.videosScrollBar}>
+    {/* Scroll Bar */}
+    <div className={styles.videosScrollBar}>
         <div
           className={styles.videosScrollBarLeft}
           // onClick={(e) => handleScroll("left")}
@@ -162,7 +164,7 @@ export default function Videos() {
         <div className={styles.videosScrollBarCenter}>
           {videoData
             ? videoData.length
-              ? `${selectedSnap + 1}/${videoData.length} ${
+              ? `${selectedSnap+1}/${videoData.length} ${
                   videoData[selectedSnap]?.name
                 }`
               : "No video is available"
